e2550125cabc6b009e4891788f79c5fee492910b
Gugu <gugunnn@gmail.com>
2025-06-29 07:01:13 +0200

    removed old project dir

--- BACKUP_28_JUNE/ARCHIVE/DELETEroutes/index.js
+++
@@ -1,7 +0,0 @@
-import authRoutes from './auth-routes.js';
-import socialAuthRoutes from './social-auth-routes.js';
-
-export default (app) => {
-  app.use('/auth', authRoutes);
-  app.use('/auth/social', socialAuthRoutes);
-};
--- BACKUP_28_JUNE/index.js
+++
@@ -1,4 +0,0 @@
-import { createServer } from './lib/http/server.js';
-
-const PORT = process.env.APP_PORT || 3000;
-createServer(PORT);
