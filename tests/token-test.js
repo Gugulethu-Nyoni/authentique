4402f468cc9a8a4a42db448948e0c5c37fcb07ce
Gugu <gugunnn@gmail.com>
2025-06-29 06:54:45 +0200

    removed tests from git tracks

--- tests/token-test.js
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE3LCJlbWFpbCI6Imd1Z3Vubm5AZ21haWwuY29tIiwiaWF0IjoxNzUxMTM1MzgxLCJleHAiOjE3NTEyMjE3ODF9.PA-kvQSL5BI1PMSqnGmmwjjQtECBr6I1FksJeDZqyno';
-const decoded = jwt.decode(token);
-console.log('Decoded JWT:', decoded);
